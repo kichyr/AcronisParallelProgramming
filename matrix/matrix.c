5ec626d0c343a4fe255af3897e1ebf1a6d60e020
kichyr <kichyr@mail.ru>
2020-05-20 01:11:48 +0300

    fixing matrix task

--- matrix/matrix.c
+++ matrix/matrix.c
@@ -4,23 +4,17 @@
 #include <pthread.h>
 
 #define M(mA, x, y) (mA[(x) * N + (y)])
+#define MIN(X, Y) (((X) < (Y)) ? (X) : (Y))
+#define MAX(X, Y) (((X) > (Y)) ? (X) : (Y))
 
-void multiply(int M, int N, int K, const double * A, const double * B, double * C)
-{
-    for (int i = 0; i < M; ++i)
-    {
-        double * c = C + i * N;
-        for (int j = 0; j < N; ++j)
-            c[j] = 0;
-        for (int k = 0; k < K; ++k)
-        {
-            const double * b = B + k * N;
-            double a = A[i*K + k];
-            for (int j = 0; j < N; ++j)
-                c[j] += a * b[j];
-        }
-    }
-}
+typedef struct multiply_worker_args {
+    int M;
+    int N;
+    int K;
+    const double * A;
+    const double * B;
+    double * C;
+} multiply_worker_args;
 
 void printMatrix(int N, int M, const double * A) {
     for (int i = 0; i < N; i++) {
@@ -31,13 +25,59 @@ void printMatrix(int N, int M, const double * A) {
     printf("---------\n");
 }
 
+void multiplyWorker(void* args) {
+    struct multiply_worker_args * m_args = (struct multiply_worker_args *)args;
+    for (int i = 0; i < m_args->N; ++i) {
+        double * c = m_args->C + i * m_args->M;
+        for (int j = 0; j < m_args->M; ++j)
+            c[j] = 0;
+        for (int k = 0; k < m_args->K; ++k)
+        {
+            const double * b = m_args->B + k * m_args->K;
+            double a = m_args->A[i*m_args-> K + k];
+            for (int j = 0; j < m_args->M; ++j)
+                c[j] += a * b[j];
+        }
+    }
+}
+
+void multiply(int TN, int M, int N, int K, const double * A, const double * B, double * C) {
+    TN = MIN(TN, N);
+    int ThreadNumber = (N % TN != 0) ? TN + 1 : TN;
+    ThreadNumber = MAX(ThreadNumber, 0);
+    multiply_worker_args * args = (multiply_worker_args *)malloc(ThreadNumber * sizeof(multiply_worker_args));
+    pthread_t* threads = (pthread_t *)malloc(ThreadNumber * sizeof(pthread_t));
+    int prev_row = 0, next_row = 0;
+    int used_processes = 0;
+    for (int i = 0; next_row != N; i++, used_processes++) {
+        next_row = MIN(prev_row + N / TN, N);
+        args[i].M = M;
+        args[i].K = K;
+        args[i].B = B;
+        args[i].N = next_row - prev_row;
+        args[i].A = A + prev_row * K;
+        args[i].C = C + prev_row * K; 
+        int err = pthread_create(&threads[i], NULL, (void *)&multiplyWorker, &args[i]);
+        if (err != 0)
+            printf("\ncan't create thread :[%s]", strerror(err));
+        prev_row = next_row;
+    }
+    
+    for (int i = 0; i < used_processes; i++) {
+        pthread_join(threads[i], NULL);
+    }
+    free(threads);
+    free(args);
+}
+
 int main(int argc, char *argv[]) {
-    if (argc != 2) {
-		fprintf(stderr, "Usage: ./main <square_matrix_size>\n");
+    if (argc != 3) {
+        printf("not enoughs args");
 		return 1;
 	}
     // row number
     int N = atoi(argv[1]);
+    int ThreadNumber = atoi(argv[2]);
     double* A = (double*)malloc(N * N * sizeof(A[0]));
 	double* B = (double*)malloc(N * N * sizeof(B[0]));
 	double* C = (double*)malloc(N * N * sizeof(C[0]));
@@ -49,7 +89,10 @@ int main(int argc, char *argv[]) {
 		B[i] = rand();
 
 	for (int i = 0; i < (N * N); i++)
-		C[i] = 0;
+		C[i] = i;
 
-    multiply(N, N, N, A, B, C);
+    multiply(ThreadNumber, N, N, N, A, B, C);
+    free(A);
+    free(B);
+    free(C);
 }
--- matrix/plot.sh
+++ matrix/plot.sh
@@ -9,19 +9,19 @@ rm -f plot.dat
 
 for B in 1 2 4 8 16
 do
-	if (($B <= 8)); then
-		echo -n "$B `bc <<< "$N*$N*$N/$B"` " >> plot.dat
-	else
-		echo -n "$B `bc <<< "$N*$N*$N/8"` " >> plot.dat
-	fi
-	perf stat -e $PERF_CACHE_MISS_EVENT_NAME ./main <<< "$B" 2>&1 \
-		| awk 'NR==4 {print $1}' >> plot.dat
+	echo -n "$B  " >> plot.dat
+	perf stat -e $PERF_CACHE_MISS_EVENT_NAME ./main "`bc <<< "2^10"`" "$B" 2>&1 \
+		| awk 'NR==6 || NR==4 {print $1}' | tr \\n ' ' >> plot.dat
+	echo "" >> plot.dat
 done
 
 
 	WIDTH=80
 	HEIGHT=25
 
+#gnuplot <<< "set term png size 1920,1080; \
+#            set output '../bench_results/c_miss_matrix.png'; \
+#            plot 'plot.dat' u 1:3 title 'cache-misses(matrix size)' w linespoints"
 gnuplot <<< "set term png size 1920,1080; \
             set output '../bench_results/matrix.png'; \
-            plot 'plot.dat' u 1:2 title 'cache-misses(matrix size)' w linespoints"
+            plot 'plot.dat' u 1:3 title 'time(thread number)' w linespoints"
